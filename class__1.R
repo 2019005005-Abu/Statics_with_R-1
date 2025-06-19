b182bb938b5cf0b1d9b7c6614a81f910064a64bd
Abu Al Shahriar Rifat <79824559+2019005005-Abu@users.noreply.github.com>
2025-06-19 21:04:21 +0600

    Add files via upload

---
+++ class__1.R
@@ -0,0 +1,79 @@
+print("I am a R Programmer")
+x=10;
+print(x)
+print('Machine Learning Enginner')
+typeof(c(3L,4L,5L))
+typeof(c(3L,"male",10L))
+
+y=10;
+z=20;
+print(y+z)
+m1<-46;
+print(m1)
+m2<-45;
+print(m1+m2);
+
+a1<-c(23,26,29,31,33)
+b1<-c(1,2,3,4,5)
+print(a1)
+print(b1)
+print(a1+b1);
+print(a1-b1);
+print(a1/b1);
+print(a1*b1)
+print(a<<-19)
+m<-1:1000;
+print(m);
+
+#Index will be started from 1 in R programming
+#//Index will be started from 0 in Python Programming
+print(a1^b1);
+print(a1%%b1)
+print(a1%/%b1);
+print(a1*b1);
+m1<-matrix(c(1,2,3,4,5,6),ncol=3,nrow=2)
+m2<-matrix(c(3,4,5),ncol=1,nrow=1)
+print(m1)
+print(m2);
+#####print(m1%*%m2)
+print(10>6)
+print("shahriar" %in% c("Abu Al shahriar"));
+x1=c('Apple','Nokia','Jack')
+"Apple" %in% x1;
+"Nokia" %in% x1;
+"Nokiaa" %in% x1;
+x1<-c(T | F)
+x2<-c(T|T)
+print(x1&x2)
+print(sum(m1))
+print(sum(a1))
+print(sum(b1))
+print(summary(mtcars))
+mss=c(23,24,25,NA,19,30);
+print(mean(mss))
+print(any(is.na(mss)))
+all(c(T,T,T,F))
+all(c(T,T,T,T))
+any(c(T,F,T,F))
+print(sum(m1))
+################################
+###############################
+x=c(1,2,3,4,5,6,7);
+x<-c(1,23,35,56,67);
+y<-c(10,20,30,40,50);
+print(x);
+print(y);
+######<----------->##########
+p<-c(1,2,3,4,5);
+q<-c(6,7,8,9,10);
+a<-c(10,11,12,14);
+print(a);
+print(q);
+print(p);
+round(c(23.45757,34.5454))
+
+f=3.45
+floor(f)
+ceiling(f)
+sqrt(25);
+help(any)
